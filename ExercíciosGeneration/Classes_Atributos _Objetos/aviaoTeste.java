3c827022722c244ee119ac1bf0c27527524e23a0
Kaciany Ramos <Kaciany2012@hotmail.com>
2021-12-25 19:58:10 -0300

    exercícios atualizados

--- ExercíciosGeneration/Classes_Atributos _Objetos/aviaoTeste.java
+++ ExercíciosGeneration/Classes_Atributos _Objetos/aviaoTeste.java
@@ -4,14 +4,8 @@ public class aviaoTeste {
 
 	public static void main(String[] args) {
 		
-		// metodos são ações de um objeto
-		/*private String modelo;
-	private String cor;
-	private String ano;
-	private String compahia;
-	private float peso;
-		 * 
-		 */
+	
+		 
 		
 		Aviao aviao1= new Aviao("Planador AC-21 ","Branco",2006,"Aeros",310);
 		
