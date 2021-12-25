59d538caac5655b730e7c39da572c1674fd33df6
Kaciany Ramos <Kaciany2012@hotmail.com>
2021-12-25 19:49:49 -0300

    exercícios atualizados

---
+++ ExercíciosGeneration/aviaoTeste.java
@@ -0,0 +1,23 @@
+package ExercíciosGeneration;
+
+public class aviaoTeste {
+
+	public static void main(String[] args) {
+		
+		// metodos são ações de um objeto
+		/*private String modelo;
+	private String cor;
+	private String ano;
+	private String compahia;
+	private float peso;
+		 * 
+		 */
+		
+		Aviao aviao1= new Aviao("Planador AC-21 ","Branco",2006,"Aeros",310);
+		
+		aviao1.imprimirInfo();
+		
+
+	}
+
+}
